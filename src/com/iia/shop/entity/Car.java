e3cdcedc8f08d58c62ca3c05e4e530cb0c5287ba
Dorian Fouillen <fouillen.dorian@orange.fr>
2016-09-29 11:08:31 +0200

    initial project version

---
+++ src/com/iia/shop/entity/Car.java
@@ -0,0 +1,10 @@
+package com.iia.shop.entity;
+
+public class Car {
+	private String make;
+	private int year;
+	private String model;
+	private String color;
+	private Double prix;
+	private int speed;
+}
---
+++ src/com/iia/shop/entity/Store.java
@@ -0,0 +1,8 @@
+package com.iia.shop.entity;
+
+public class Store {
+	public static void main(String[] args){
+		
+		
+	}
+}
