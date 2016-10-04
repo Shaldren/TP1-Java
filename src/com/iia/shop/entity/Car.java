61de6edbd87a55f9a62a9aaa56d090fa7acbfcb2
Shaldren <fouillen.dorian@orange.fr>
2016-10-04 19:11:49 +0200

    Add files via upload

--- src/com/iia/shop/entity/Car.java
+++ src/com/iia/shop/entity/Car.java
@@ -7,4 +7,76 @@ public class Car {
 	private String color;
 	private Double prix;
 	private int speed;
+	
+public Car() {
+	super();
+}
+
+
+public Car(String make, int year, String model, String color,Double prix,int speed) {
+	super();
+	this.make = make;
+	this.year = year;
+	this.model = model;
+	this.color = color;
+	this.prix = prix;
+	this.speed = speed;
+	}
+
+public String getMake(){
+	return this.make;
+}
+public void setMake(String make){
+	this.make = make;
+}
+
+public void reader(){
+	System.out.println("Marque et Modele : " + getMake() + " " + getModel() + "\n" 
+			+ "Annee : " + getYear() + "\n"
+			+ "Couleur : " + getColor() + "\n"
+			+ "Prix : " + getPrix() + "\n"
+			+ "Vitesse : " + getSpeed());
+}
+
+public int getYear() {
+	return year;
+}
+
+public void setYear(int year) {
+	this.year = year;
+}
+
+public String getModel() {
+	return model;
+}
+
+public void setModel(String model) {
+	this.model = model;
+}
+
+public String getColor() {
+	return color;
+}
+
+public void setColor(String color) {
+	this.color = color;
+}
+
+public Double getPrix() {
+	return prix;
+}
+
+public void setPrix(Double prix) {
+	this.prix = prix;
+}
+
+public int getSpeed() {
+	return speed;
+}
+
+public void setSpeed(int speed) {
+	this.speed = speed;
+}
+
+
 }
--- src/com/iia/shop/entity/Store.java
+++ src/com/iia/shop/entity/Store.java
@@ -1,8 +1,60 @@
+
 package com.iia.shop.entity;
+import java.io.Console;
+import java.util.ArrayList;
+
+import com.iia.shop.entity.Car;
+
+
+
 
 public class Store {
 	public static void main(String[] args){
+		Car c1 = new Car("Peugeot", 2004, "206", "Red", 1599.99, 210);
+		Car c2= new Car("Renault", 1996, "Super 5", "White", 899.99, 180);
 		
+		int programme = 1;
+		Console console = System.console();
 		
+		ArrayList<Car> Carlist = new ArrayList<Car>();
+		
+		Carlist.add(c1);
+		Carlist.add(c2);
+		while (programme != -1 && programme < 7) {
+			
+			
+			int n ;
+
+			System.out.println("1. Afficher la liste");
+			System.out.println("2. Modifier");
+			System.out.println("3. Supprimer");
+			
+			n = Integer.parseInt(console.readLine(" Entrer une valeur : "));
+			
+			switch (n) {
+			case 1:
+				System.out.println("Liste des voiture");
+				
+				for (Car o : Carlist){ 
+					System.out.print("\n" + o.getMake()+ " " + o.getModel() + "\n");
+					}
+				break;
+			case 2:
+				System.out.println("Ajout");
+				break;
+			case 3:
+				System.out.println("Modification");
+				break;
+			case 4:
+				System.out.println("Suppression");
+				break;
+			default:
+				System.out.println("Vous avez quitté l'application");
+				break;
+			}
+			
+		System.out.println("Voulez vous continuer d'utiliser la liste ? 0 = oui / -1 = non");
+		programme = Integer.parseInt(console.readLine(" Entrer une valeur : "));
+	}
 	}
 }
